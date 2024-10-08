35d0afd6528a3ba8778391a8a5d9140a379e6806
mwanzaalbert <user.email>
2024-10-08 12:05:35 +0300

    pycodestyle formatting

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,23 +17,22 @@ def canUnlockAll(boxes):
     """
     # Initialize a set to keep track of unlocked boxes, starting with box 0
     unlocked_boxes = set([0])
-    
+
     # Create a list to track keys we have found
     keys = set(boxes[0])
-    
+
     # Add keys from the initially unlocked box
     while keys:
         key = keys.pop()
         if key < len(boxes) and key not in unlocked_boxes:
             unlocked_boxes.add(key)
-            
+
             # Add new keys from this newly unlocked box
             keys.update(boxes[key])
-    
+
     # If the number of unlocked boxes is equal to the total number of boxes,
     # return True
     return len(unlocked_boxes) == len(boxes)
-
 
 
 if __name__ == "__main__":
